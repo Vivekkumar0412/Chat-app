867b4bd9cb3f9fc199a90e23c8526bb4b1ba5689
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2026-01-15 15:45:19 +0530

    new file added

---
+++ backend/src/socket.ts
@@ -0,0 +1,36 @@
+import { WebSocketServer, WebSocket, type RawData } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+type HandlerFun = (payload: unknown) => void;
+const handler = new Map<string, HandlerFun>();
+wss.on("connection", (socket: WebSocket) => {
+    console.log("Client connected");
+
+    socket.on("message", (data: RawData) => {
+        try {
+            const parsedData = JSON.parse(data.toString());
+            if(!parsedData.type){
+                console.log("invalid type");
+                return;
+            };
+
+            if(!parsedData.payload || typeof !parsedData.payload == "string"){
+                console.log("invalid payload");
+                return;
+            }
+
+        } catch (error) {
+            console.log("some error occured",error);
+            socket.close();
+        }
+
+    });
+
+    socket.on("close", () => {
+        console.log("Client disconnected")
+    });
+
+    socket.on("error", (error: Error) => {
+        console.log("some error occured", error)
+    })
+})
